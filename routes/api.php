185f5d31cefc29dcc8e5ce57ca025a67005d88f3
Ilham4444 <ilhamelabyad4@gmail.com>
2026-08-14 21:49:15 +0200

    Routes API mises a jour pour pointer vers les Controllers Api/

--- routes/api.php
+++ routes/api.php
@@ -1,108 +1,41 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Actions\PaymentOrder\CreatePaymentOrder;
-use App\Actions\PaymentOrder\ListPaymentOrders;
-use App\Actions\PaymentOrder\ShowPaymentOrder;
-use App\Actions\PaymentOrder\PayPaymentOrder;
-use App\Actions\PaymentOrder\CancelPaymentOrder;
-use App\Actions\Payment\ListPayments;
-use App\Actions\Payment\ShowPayment;
-use App\Actions\Platform\CreatePlatform;
-use App\Actions\Platform\ListPlatforms;
-use App\Actions\Platform\ShowPlatform;
-use App\Actions\Platform\UpdatePlatform;
-use App\Actions\Platform\DeletePlatform;
-use App\Actions\Service\CreateService;
-use App\Actions\Service\ListServices;
-use App\Actions\Service\ShowService;
-use App\Actions\Service\UpdateService;
-use App\Actions\Service\DeleteService;
-use App\Actions\Objet\CreateObjet;
-use App\Actions\Objet\ListObjets;
-use App\Actions\Objet\ShowObjet;
-use App\Actions\Objet\UpdateObjet;
-use App\Actions\Objet\DeleteObjet;
-use App\Actions\Article\CreateArticle;
-use App\Actions\Article\ListArticles;
-use App\Actions\Article\ShowArticle;
-use App\Actions\Article\UpdateArticle;
-use App\Actions\Article\DeleteArticle;
-use App\Actions\PersonnePhysique\CreatePersonnePhysique;
-use App\Actions\PersonnePhysique\ListPersonnePhysiques;
-use App\Actions\PersonnePhysique\ShowPersonnePhysique;
-use App\Actions\PersonnePhysique\UpdatePersonnePhysique;
-use App\Actions\PersonnePhysique\DeletePersonnePhysique;
-use App\Actions\Association\CreateAssociation;
-use App\Actions\Association\ListAssociations;
-use App\Actions\Association\ShowAssociation;
-use App\Actions\Association\UpdateAssociation;
-use App\Actions\Association\DeleteAssociation;
-use App\Actions\Societe\CreateSociete;
-use App\Actions\Societe\ListSocietes;
-use App\Actions\Societe\ShowSociete;
-use App\Actions\Societe\UpdateSociete;
-use App\Actions\Societe\DeleteSociete;
-use App\Http\Controllers\TestController;
+use App\Http\Controllers\testController;
+use App\Http\Controllers\Api\PlatformController;
+use App\Http\Controllers\Api\ServiceController;
+use App\Http\Controllers\Api\ObjetController;
+use App\Http\Controllers\Api\ArticleController;
+use App\Http\Controllers\Api\PersonnePhysiqueController;
+use App\Http\Controllers\Api\AssociationController;
+use App\Http\Controllers\Api\SocieteController;
+use App\Http\Controllers\Api\PaymentOrderController;
+use App\Http\Controllers\Api\PaymentController;
 
 
-Route::get('/platforms', ListPlatforms::class);
-Route::post('/platforms', CreatePlatform::class);
-Route::get('/platforms/{platform}', ShowPlatform::class);
-Route::put('/platforms/{platform}', UpdatePlatform::class);
-Route::delete('/platforms/{platform}', DeletePlatform::class);
+
+Route::apiResource('platforms', PlatformController::class);
+Route::apiResource('services', ServiceController::class);
+Route::apiResource('objets', ObjetController::class);
+Route::apiResource('articles', ArticleController::class);
+Route::apiResource('personne-physiques', PersonnePhysiqueController::class);
+Route::apiResource('associations', AssociationController::class);
+Route::apiResource('societes', SocieteController::class);
 
 
-Route::get('/services', ListServices::class);
-Route::post('/services', CreateService::class);
-Route::get('/services/{service}', ShowService::class);
-Route::put('/services/{service}', UpdateService::class);
-Route::delete('/services/{service}', DeleteService::class);
+Route::get('/payment-orders', [PaymentOrderController::class, 'index']);
+Route::post('/payment-orders', [PaymentOrderController::class, 'store']);
+Route::get('/payment-orders/{paymentOrder}', [PaymentOrderController::class, 'show']);
+Route::post('/payment-orders/{paymentOrder}/pay', [PaymentOrderController::class, 'pay']);
+Route::post('/payment-orders/{paymentOrder}/cancel', [PaymentOrderController::class, 'cancel']);
 
 
-Route::get('/objets', ListObjets::class);
-Route::post('/objets', CreateObjet::class);
-Route::get('/objets/{objet}', ShowObjet::class);
-Route::put('/objets/{objet}', UpdateObjet::class);
-Route::delete('/objets/{objet}', DeleteObjet::class);
 
-
-Route::get('/articles', ListArticles::class);
-Route::post('/articles', CreateArticle::class);
-Route::get('/articles/{article}', ShowArticle::class);
-Route::put('/articles/{article}', UpdateArticle::class);
-Route::delete('/articles/{article}', DeleteArticle::class);
-
-
-Route::get('/payment-orders', ListPaymentOrders::class);
-Route::post('/payment-orders', CreatePaymentOrder::class);
-Route::get('/payment-orders/{paymentOrder}', ShowPaymentOrder::class);
-Route::post('/payment-orders/{paymentOrder}/pay', PayPaymentOrder::class);
-Route::post('/payment-orders/{paymentOrder}/cancel', CancelPaymentOrder::class);
-
-
-Route::get('/payments', ListPayments::class);
-Route::get('/payments/{payment}', ShowPayment::class);
-
-Route::get('/personne-physiques', ListPersonnePhysiques::class);
-Route::post('/personne-physiques', CreatePersonnePhysique::class);
-Route::get('/personne-physiques/{personnePhysique}', ShowPersonnePhysique::class);
-Route::put('/personne-physiques/{personnePhysique}', UpdatePersonnePhysique::class);
-Route::delete('/personne-physiques/{personnePhysique}', DeletePersonnePhysique::class);
-
-Route::get('/associations', ListAssociations::class);
-Route::post('/associations', CreateAssociation::class);
-Route::get('/associations/{association}', ShowAssociation::class);
-Route::put('/associations/{association}', UpdateAssociation::class);
-Route::delete('/associations/{association}', DeleteAssociation::class);
-
-Route::get('/societes', ListSocietes::class);
-Route::post('/societes', CreateSociete::class);
-Route::get('/societes/{societe}', ShowSociete::class);
-Route::put('/societes/{societe}', UpdateSociete::class);
-Route::delete('/societes/{societe}', DeleteSociete::class);
+Route::get('/payments', [PaymentController::class, 'index']);
+Route::get('/payments/{payment}', [PaymentController::class, 'show']);
 
 
 Route::get('/test/platform', [TestController::class, 'testCreatePlatform']);
+Route::get('/test/chain', [TestController::class, 'testCreateChain']);
 Route::get('/test/full-flow', [TestController::class, 'testFullFlow']);
 Route::get('/test/cancel/{paymentOrder}', [TestController::class, 'testCancel']);
